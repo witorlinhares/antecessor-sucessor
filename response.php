28723ab84cec56832cedc64830dcfc3e73a0eb3c
witorlinhares <154279407+witorlinhares@users.noreply.github.com>
2024-02-28 11:46:42 -0300

    style and logic

--- response.php
+++ response.php
@@ -1,26 +1,33 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Antecessor e Sucessor</title>
+    <link rel="stylesheet" href="assets/style.css">
+    <link rel="icon" href="assets/favicon.png" type="image/x-icon">
 </head>
+
 <body>
-        <main>
-            <h1>Este é o resultado</h1>
-            <p>
+    <main>
+        <h1>Este é o resultado:</h1>
+        <p>
 
             <?php
             $n = $_GET["num"]  ?? 0;
             $a = $n - 1;
             $s = $n + 1;
-            echo "O número escolhido foi $n";
-            echo "<br>O seu antecessor é $a";
-            echo "<br>O seu sucessor é $s";
+            echo "O número escolhido foi <strong>$n</strong>";
+            echo "<br>O seu antecessor é <strong>$a</strong>";
+            echo "<br>O seu sucessor é <strong>$s</strong>";
 
 
             ?>
-        </main>
-    
+        </p>
+        <button onclick="javascript:history.go(-1)">Voltar</button>
+    </main>
+
 </body>
+
 </html>
